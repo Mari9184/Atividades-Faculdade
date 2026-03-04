927254af54939036fd4d6ef9800eefa2880be5bf
Mariane Santana da Silva <marianess638@gmail.com>
2026-03-04 15:46:34 -0300

    Enhance Main class with more exercises
    Added additional exercises for user input and calculations.

--- main.java
+++ main.java
@@ -1,32 +1,6 @@
 //Mariane Santana da Silva RA:1143431607
 
-class Main {
-    public static void main (String [] args){
-
-        //Variáveis para os exercícios
-
-        int Idade=18, RA=1143431607, num=5;
-        double Altura=1.61;
-        String Nome="Mariane Santana da Silva",Status= "Presente", Curso="Tecnico em Analise e Desenvolvimento de Sistemas";
-
-        // 1° Exercício
-
-        System.out.printf("\nMeu nome é %s, Estou cursando %s e tenho por RA: %d\n",Nome,Curso,RA);
-
-        // 2° Exercício
-
-        System.out.printf("\nTenho %d anos e %f de altura e estive %s na aula de AP1 no dia 25.02.2026\n",Idade, Altura,Status);
-
-        //  3° Exercício com printf
-
-        System.out.println("\nTABUADA DO 5");
-        for (int i=1 ; i <=10; i++){System.out.printf("%d * %d = %d%n",num,i,num*i);}
-
-    }
-}
-
-//Mariane Santana da Silva RA:1143431607
-
+import java.lang.Math;
 import java.util.Scanner;
 class Main {
     public static void main (String [] args){
@@ -146,6 +120,17 @@ class Main {
 
         //Exercício 13
 
+        System.out.println("Imforme um número: "); 
+        double num =e.nextDouble();
+        double Q=num*num;
+        double cubo=num*num*num;
+        double raiz=Math.sqrt(num);
+        double potencia=Math.pow(num, 10);
+        System.out.println("O quadrado deste numero é: "+quadrado);
+        System.out.println("O cubo deste numero é: "+cubo);
+        System.out.println("A raiz quadrada deste numero é: "+raiz);
+        System.out.println("Este numero elevado a decima potencia é: "+potencia);
+
 
 
 
@@ -153,3 +138,4 @@ class Main {
 
 
     }
+
