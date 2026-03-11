1f62e62a5e5f9364fc6041b14dea942630aaba90
Mariane Santana da Silva <marianess638@gmail.com>
2026-03-11 15:11:23 -0300

    Add Exercicio_11.java to calculate circle area

---
+++ AlgoritmoProgramação1/ListaExercicio_1/Exercicio_11.java
@@ -0,0 +1,15 @@
+//Mariane Santana da Silva RA:1143431607
+
+import java.util.Scanner;
+
+class Main {
+    public static void main (String [] args){
+        Scanner ler = new Scanner(System.in);
+        
+        // Exercício 11
+
+        double Raio = 5;
+        double Circulo = 3.14*(Raio*Raio);
+        System.out.printf("\n A área do círculo é:"+Circulo);
+    }
+}
