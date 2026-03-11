6c7dced5946100f8aff853b48cf8add9bc496800
Mariane Santana da Silva <marianess638@gmail.com>
2026-03-11 15:06:26 -0300

    Implement average calculation in Exercici9.java
    Fix calculation of average in Exercise 9.

---
+++ AlgoritmoProgramação1/ListaExercicio_1/Exercici9.java
@@ -0,0 +1,12 @@
+//Mariane Santana da Silva RA:1143431607
+
+class Main {
+    public static void main (String [] args){
+
+        // Exercício 9
+
+        double M = 8.0+7.5+4.5+9.0/4;
+        System.out.printf("\nSua media é:"+M);
+
+    }
+}
