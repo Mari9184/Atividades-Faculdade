464030ee752531f3a6aabed88a3a179ba5d99e18
Mariane Santana da Silva <marianess638@gmail.com>
2026-03-11 11:14:28 -0300

    Add Exercise 4 for comparing and printing numbers
    Implement Exercise 4 to compare two numbers and print them in order.

---
+++ AlgoritmoProgramação1/ListaExercicios_2/Exercicio4.java
@@ -0,0 +1,22 @@
+//Mariane Santana da Silva RA:1143431607
+
+import java.util.Scanner;
+class Main {
+    public static void main (String [] args) {
+        Scanner ler = new Scanner(System.in);
+
+            // Exercício 4
+
+        System.out.println("Informe um numero");
+        float m = ler.nextFloat();
+
+        System.out.println("Informe um numero");
+        float n = ler.nextFloat();
+
+        if (m > n) {
+            System.out.printf(" %f,%f", x, y);
+        } else if (n >= m) {
+            System.out.printf(" %f,%f", y, x);
+        }
+    }
+}
