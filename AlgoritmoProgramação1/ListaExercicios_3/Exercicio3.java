dfa7ff100cd25a4df16a7a3db4d3cea329b30c0c
Mariane Santana da Silva <marianess638@gmail.com>
2026-03-14 15:19:12 -0300

    Add Exercicio3.java for period greeting program

---
+++ AlgoritmoProgramação1/ListaExercicios_3/Exercicio3.java
@@ -0,0 +1,32 @@
+//Mariane Santana da Silva RA:1143431607
+
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+
+        // Exercicio 3
+        
+        System.out.println("Em que periodo você estuda?\n M - Matutino \n V - Vespertino \n N - Noturno");
+        String P = ler.nextLine();
+
+        switch (P) {
+            case "M":
+                System.out.print("Bom dia!");
+            break;
+            
+            case "V":
+                System.out.print("Boa tarde!");
+            break;
+            
+            case "N":
+                System.out.print("Boa Noite!");
+            break;
+            
+            default:
+                System.out.print("Não possui este periodo.");
+        }
+        
+    }
+}
